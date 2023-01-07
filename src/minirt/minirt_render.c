ea12a6400f9fac93b4b16d13ea6d6a1a06e96158
맹주영 <mjy9088@naver.com>
2023-01-07 20:59:23 +0900

    fix compilation error

--- src/minirt/minirt_render.c
+++ src/minirt/minirt_render.c
@@ -12,39 +12,7 @@
 
 #include "minirt.h"
 
-#include <unistd.h>
-
-#include <mlx.h>
-
-#include "ft_exit.h"
-#include "ft_io.h"
-
-#include "minirt_hooks.h"
-
-#define W 1920
-#define H 1080
-#define TITLE "CSG+HDR minirt!"
-
-void	minirt_init(t_minirt *minirt, int argc, char **argv)
+void	minirt_render(t_minirt *param)
 {
-	(void)argv;
-	if (argc < 2)
-	{
-		ft_write(STDOUT_FILENO, "Error: no input file\n", 21);
-		ft_exit(1);
-	}
-	minirt->mlx_context = mlx_init();
-	if (!minirt->mlx_context)
-	{
-		ft_write(STDOUT_FILENO, "Error: no input file\n", 21);
-		ft_exit(1);
-	}
-	minirt->mlx_window = mlx_new_window(minirt->mlx_context, W, H, TITLE);
-	minirt->pre_image = mlx_new_image(minirt->mlx_context, W, H);
-	minirt->final_image = mlx_new_image(minirt->mlx_context, W, H);
-	mlx_expose_hook(minirt->mlx_window, &minirt_hooks_init, minirt);
-	mlx_hook(minirt->mlx_window, MLX_EVENT_ON_DESTROY,
-		0, &minirt_hooks_exit, minirt);
-	mlx_hook(minirt->mlx_window, MLX_EVENT_ON_KEYDOWN,
-		0, &minirt_hooks_key, minirt);
+	(void)param;
 }
