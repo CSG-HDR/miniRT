da0dec651bca009f798ad9a8fbba2a90bab24cb9
yeonseong <yeonseong.dev@gmail.com>
2023-01-07 22:56:09 +0900

    feat: normize normal vector

--- src/minirt/math/minirt_math_convert.c
+++ src/minirt/math/minirt_math_convert.c
@@ -5,7 +5,7 @@ t_vec3	minirt_math_convert_to_normal(t_vec3 rotate)
 	const double theta_x = 2 * M_PI * rotate.x;
 	const double theta_y = 2 * M_PI * rotate.y;
 	const double theta_z = 2 * M_PI * rotate.z;
-
-	t_vec3 normal_vector = vec3(tan(theta_x), tan(theta_y), tan(theta_z));
-	return (normal_vector);
+	const t_vec3 norm = vec3(tan(theta_x), tan(theta_y), tan(theta_z));
+	
+	return (vunit(norm));
 }
