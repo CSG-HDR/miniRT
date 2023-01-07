7d8935c97874938e6f5d2f9c2f79191cb15b5f96
yeonseong <yeonseong.dev@gmail.com>
2023-01-08 00:46:14 +0900

    fix: tan -> cos

--- src/minirt/math/minirt_math_convert.c
+++ src/minirt/math/minirt_math_convert.c
@@ -2,10 +2,10 @@
 
 t_vec3	minirt_math_convert_to_normal(t_vec3 rotate)
 {
-	const double theta_x = 2 * M_PI * rotate.x;
-	const double theta_y = 2 * M_PI * rotate.y;
-	const double theta_z = 2 * M_PI * rotate.z;
-	const t_vec3 norm = vec3(tan(theta_x), tan(theta_y), tan(theta_z));
-	
-	return (vunit(norm));
+   		const double theta_x = 2 * M_PI * rotate.x;
+		const double theta_y = 2 * M_PI * rotate.y;
+		const double theta_z = 2 * M_PI * rotate.z;
+		const t_vec3 norm = vec3(cos(theta_x), cos(theta_y), cos(theta_z));
+		
+		return (vunit(norm));
 }
