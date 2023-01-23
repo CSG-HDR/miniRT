4f754d5b3d001590000b4d1a965ada8ce654629e
맹주영 <mjy9088@naver.com>
2023-01-23 13:09:19 +0900

    add functions' prototype for t_texture_manager

--- src/t/texture_manager/t_texture_manager.h
+++ src/t/texture_manager/t_texture_manager.h
@@ -34,12 +34,84 @@ typedef struct s_texture_manager
 }	t_texture_manager;
 
 t_texture_manager	*t_texture_manager_new(void);
-void				t_texture_manager_free(t_texture_manager *self);
+void				t_texture_manager_free(
+						t_texture_manager *self);
 t_err				t_texture_manager_preload(
-						t_texture_manager *self, const char *path);
-t_err				t_texture_manager_preload_all(
-						t_texture_manager *self, t_map *map);
+						t_texture_manager *self,
+						const char *path);
 t_image				*t_texture_manager_get(
-						t_texture_manager *self, const char *path);
+						t_texture_manager *self,
+						const char *path);
+
+t_err				t_texture_manager_preload_all(
+						t_texture_manager *self,
+						t_map *map);
+t_err				t_texture_manager_preload_models(
+						t_texture_manager *self,
+						t_map_model **models,
+						size_t count);
+t_err				t_texture_manager_preload_model(
+						t_texture_manager *self,
+						t_map_model *model);
+t_err				t_texture_manager_preload_constructive(
+						t_texture_manager *self,
+						t_map_constructive constructive);
+t_err				t_texture_manager_preload_union(
+						t_texture_manager *self,
+						t_map_union _union);
+t_err				t_texture_manager_preload_intersection(
+						t_texture_manager *self,
+						t_map_intersection intersection);
+t_err				t_texture_manager_preload_difference(
+						t_texture_manager *self,
+						t_map_difference difference);
+t_err				t_texture_manager_preload_primitive(
+						t_texture_manager *self,
+						t_map_primitive primitive);
+t_err				t_texture_manager_preload_sphere(
+						t_texture_manager *self,
+						t_map_sphere sphere);
+t_err				t_texture_manager_preload_ellipsoid(
+						t_texture_manager *self,
+						t_map_ellipsoid ellipsoid);
+t_err				t_texture_manager_preload_torus(
+						t_texture_manager *self,
+						t_map_torus torus);
+t_err				t_texture_manager_preload_cone(
+						t_texture_manager *self,
+						t_map_cone cone);
+t_err				t_texture_manager_preload_cylinder(
+						t_texture_manager *self,
+						t_map_cylinder cylinder);
+t_err				t_texture_manager_preload_cube(
+						t_texture_manager *self,
+						t_map_cube cube);
+t_err				t_texture_manager_preload_material(
+						t_texture_manager *self,
+						t_map_material material);
+t_err				t_texture_manager_preload_normal_map(
+						t_texture_manager *self,
+						t_map_normal_map normal_map);
+t_err				t_texture_manager_preload_texture(
+						t_texture_manager *self,
+						t_map_texture texture);
+t_err				t_texture_manager_preload_blend(
+						t_texture_manager *self,
+						t_map_blend blend);
+t_err				t_texture_manager_preload_blend_add(
+						t_texture_manager *self,
+						t_map_blend_add blend_add);
+t_err				t_texture_manager_preload_blend_multiply(
+						t_texture_manager *self,
+						t_map_blend_multiply blend_multiply);
+t_err				t_texture_manager_preload_blend_maximum(
+						t_texture_manager *self,
+						t_map_blend_maximum blend_maximum);
+t_err				t_texture_manager_preload_blend_minimum(
+						t_texture_manager *self,
+						t_map_blend_minimum blend_minimum);
+t_err				t_texture_manager_preload_blend_subtract(
+						t_texture_manager *self,
+						t_map_blend_subtract blend_subtract);
 
 #endif
