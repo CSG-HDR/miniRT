4bfd86a407e8ab1920f9dd075525ac75e452716c
맹주영 <mjy9088@naver.com>
2023-01-23 20:21:40 +0900

    fix incorrect quadric validation rule

--- src/t/map/validate/t_map_validate_quadric.c
+++ src/t/map/validate/t_map_validate_quadric.c
@@ -21,11 +21,13 @@ static bool	valid_list(t_ft_json value)
 	return (
 		true
 		&& t_map_validate_has_number_list(value, 0)
-		&& ft_json_get_number(ft_json_get_list(value, 0)) != 0
 		&& t_map_validate_has_number_list(value, 1)
-		&& ft_json_get_number(ft_json_get_list(value, 1)) != 0
 		&& t_map_validate_has_number_list(value, 2)
-		&& ft_json_get_number(ft_json_get_list(value, 2)) != 0
+		&& !(false
+			|| ft_json_get_number(ft_json_get_list(value, 0)) != 0
+			|| ft_json_get_number(ft_json_get_list(value, 1)) != 0
+			|| ft_json_get_number(ft_json_get_list(value, 2)) != 0
+		)
 		&& t_map_validate_has_number_list(value, 3)
 		&& t_map_validate_has_number_list(value, 4)
 		&& t_map_validate_has_number_list(value, 5)
@@ -41,11 +43,13 @@ static bool	valid_dict(t_ft_json value)
 	return (
 		true
 		&& t_map_validate_has_number_dict(value, "A")
-		&& ft_json_get_number(ft_json_get_dict(value, "A")) != 0
 		&& t_map_validate_has_number_dict(value, "B")
-		&& ft_json_get_number(ft_json_get_dict(value, "B")) != 0
 		&& t_map_validate_has_number_dict(value, "C")
-		&& ft_json_get_number(ft_json_get_dict(value, "C")) != 0
+		&& !(false
+			|| ft_json_get_number(ft_json_get_dict(value, "A")) != 0
+			|| ft_json_get_number(ft_json_get_dict(value, "B")) != 0
+			|| ft_json_get_number(ft_json_get_dict(value, "C")) != 0
+		)
 		&& t_map_validate_has_optional_number(value, "D")
 		&& t_map_validate_has_optional_number(value, "E")
 		&& t_map_validate_has_optional_number(value, "F")
