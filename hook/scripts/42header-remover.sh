a45b704e0b79e0d7f096d265e4254b49566e0b4f
맹주영 <mjy9088@naver.com>
2023-01-25 00:59:48 +0900

    resolve 42seoul header problem

--- hook/scripts/42header-remover.sh
+++ hook/scripts/42header-remover.sh
@@ -6,7 +6,7 @@ FILE_NAME="fake_file_name (file name is useless too)    "
 # FILE_NAME="$(basename $1)$(printf %45.45s)" # uncomment when norminette starts complain it
 PLACEHOLDER="PLACEHOLDER_FOR_FILE_NAME_UP_TO_45_CHARACTERS"
 
-if head -11 < "$1" | grep -Eo ".{25}$" | cmp -s - hook/assets/42header-c-form-11x25.txt; then
+if head -8 < "$1" | grep -Eo ".{25}$" | cmp -s - hook/assets/42header-c-form-8x25.txt; then
   sed "s/$PLACEHOLDER/$FILE_NAME/" < hook/assets/42header-c-template.txt > "$1.tmp"
   tail -n +12 "$1" >> "$1.tmp"
   mv "$1" "$1.bak"
@@ -15,7 +15,7 @@ if head -11 < "$1" | grep -Eo ".{25}$" | cmp -s - hook/assets/42header-c-form-11
   git add "$1"
 fi
 
-if head -11 < "$1" | grep -Eo ".{25}$" | cmp -s - hook/assets/42header-t-form-11x25.txt; then
+if head -8 < "$1" | grep -Eo ".{25}$" | cmp -s - hook/assets/42header-t-form-8x25.txt; then
   sed "s/$PLACEHOLDER/$FILE_NAME/" < hook/assets/42header-t-template.txt > "$1.tmp"
   tail -n +12 "$1" >> "$1.tmp"
   mv "$1" "$1.bak"
