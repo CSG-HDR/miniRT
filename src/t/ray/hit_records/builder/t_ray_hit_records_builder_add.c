fddbad9389947abc2dc14179c3a3b7580c0b5f15
Juyeong Maing <mjy9088@naver.com>
2023-01-30 10:43:04 +0900

    add t_ray_hit_records_builder zero thick guard

--- src/t/ray/hit_records/builder/t_ray_hit_records_builder_add.c
+++ src/t/ray/hit_records/builder/t_ray_hit_records_builder_add.c
@@ -12,17 +12,53 @@
 
 #include "t_ray.h"
 
+#include <stdbool.h>
+
 #include "ft_types.h"
 #include "wrap.h"
+
+static bool	remove_duplicate_record(
+	t_ray_hit_records_builder *self,
+	t_ray_hit_record record
+)
+{
+	t_ray_hit_records_builder_node	*prev;
+	t_ray_hit_records_builder_node	*to_remove;
+
+	if (self->head && self->head->record.distance == record.distance
+		&& self->head->record.is_front_face != record.is_front_face)
+	{
+		to_remove = self->head;
+		self->head = to_remove->next;
+		wrap_free(to_remove);
+		return (true);
+	}
+	prev = self->head;
+	while (prev && prev->next)
+	{
+		if (prev->next->record.distance == record.distance
+			&& self->head->record.is_front_face != record.is_front_face)
+		{
+			to_remove = prev->next;
+			prev->next = to_remove->next;
+			wrap_free(prev->next);
+			return (true);
+		}
+		prev = prev->next;
+	}
+	return (false);
+}
 
 t_err	t_ray_hit_records_builder_add(
 	t_ray_hit_records_builder *self,
 	t_ray_hit_record record
 )
 {
-	t_ray_hit_records_builder_node *const	node
-		= wrap_malloc(sizeof(t_ray_hit_records_builder_node));
+	t_ray_hit_records_builder_node	*node;
 
+	if (remove_duplicate_record(self, record))
+		return (false);
+	node = wrap_malloc(sizeof(t_ray_hit_records_builder_node));
 	if (!node)
 		return (true);
 	node->next = NULL;
