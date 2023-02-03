60f48f32a255965efa506f89a6e843d219ef1b96
Juyeong Maing <mjy9088@naver.com>
2023-02-03 21:03:54 +0900

    add t_ray_primitive_sphere

--- src/t/ray/primitive/t_ray_primitive_sphere.c
+++ src/t/ray/primitive/t_ray_primitive_sphere.c
@@ -15,15 +15,18 @@
 #include "ft_types.h"
 #include "t_map.h"
 
-// TODO: implement
 t_err	t_ray_primitive_sphere(
 	t_ray ray,
 	t_map_sphere sphere,
 	t_ray_hit_records *out
 )
 {
-	(void)ray;
-	(void)sphere;
-	*out = (t_ray_hit_records){0, NULL};
-	return (false);
+	const t_map_ellipsoid	as_ellipsoid = {
+		sphere.position,
+		(t_map_size){sphere.radius, sphere.radius, sphere.radius},
+		(t_map_rotation){0, 0, 0},
+		sphere.material,
+	};
+
+	return (t_ray_primitive_ellipsoid(ray, as_ellipsoid, out));
 }
