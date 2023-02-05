f6c9397e9648f28a615e9280a3b4abec2f9dc91f
맹주영 <mjy9088@naver.com>
2023-02-05 15:18:20 +0900

    fix ellipsoid normal

--- src/t/ray/primitive/ellipsoid/t_ray_primitive_ellipsoid_normal.c
+++ src/t/ray/primitive/ellipsoid/t_ray_primitive_ellipsoid_normal.c
@@ -15,9 +15,34 @@
 #include "t_f3.h"
 #include "t_map.h"
 
-t_map_normal	t_ray_primitive_ellipsoid_normal(
-	t_map_position point
-)
+/*
+ * f(x) = Ax^2 + By^2 + Cz^2 - 1 = 0
+ * f'(x) = 2Ax
+ */
+static t_f	x(t_vars l, t_map_position point)
 {
-	return (t_f3_unit(point));
+	return (2 * l.a * point.x);
+}
+
+/*
+ * f(y) = Ax^2 + By^2 + Cz^2 - 1 = 0
+ * f'(y) = 2By
+ */
+static t_f	y(t_vars l, t_map_position point)
+{
+	return (2 * l.b * point.y);
+}
+
+/*
+ * f(z) = Ax^2 + By^2 + Cz^2 - 1 = 0
+ * f'(z) = 2Cz
+ */
+static t_f	z(t_vars l, t_map_position point)
+{
+	return (2 * l.c * point.z);
+}
+
+t_map_normal	t_ray_primitive_ellipsoid_normal(t_vars l, t_map_position point)
+{
+	return (t_f3_unit((t_map_normal){x(l, point), y(l, point), z(l, point)}));
 }
--- src/t/ray/primitive/ellipsoid/t_ray_primitive_ellipsoid_record.c
+++ src/t/ray/primitive/ellipsoid/t_ray_primitive_ellipsoid_record.c
@@ -19,44 +19,9 @@
 #include "t_map.h"
 #include "t_ray.h"
 
-/*
- * f(x, y, z) = x^2/a^2 + y^2/b^2 + z^2/c^2 - 1 = 0
- * f(ray.position + ray.direction * distance) = 0
- * f(p+ut, q+vt, r+wt) = 0
- *
- * A = 1/a^2, B = 1/b^2, C = 1/c^2
- * Ax^2 + By^2 + Cz^2 - 1 = 0
- * Ap^2 + 2Aput + Au^2t^2 + Bq^2 + 2Bqvt + Bv^2t^2 + Cr^2 + 2Crwt + Cw^2t^2 -1=0
- * X = Au^2 + Bv^2 + Cw^2
- * Y = 2Apu + 2Bqv + 2Crw
- * Z = Ap^2 + Bq^2 + Cr^2 - 1
- * t = (-Y +- sqrt(Y^2 - 4XZ)) / 2X
- */
-typedef struct s_locals
+static t_vars	s_vars(t_ray ray, t_map_ellipsoid ellipsoid)
 {
-	t_f				a;
-	t_f				b;
-	t_f				c;
-	t_f				p;
-	t_f				q;
-	t_f				r;
-	t_f				u;
-	t_f				v;
-	t_f				w;
-	t_f				x;
-	t_f				y;
-	t_f				z;
-	t_f				y2_4xz;
-	bool			hit;
-	bool			has_record;
-	bool			in_ellipsoid;
-	t_f				sqrt_y2_4xz;
-	t_map_ellipsoid	self;
-}	t_locals;
-
-static t_locals	s_locals(t_ray ray, t_map_ellipsoid ellipsoid)
-{
-	t_locals	l;
+	t_vars	l;
 
 	l.a = 1 / (ellipsoid.size.x * ellipsoid.size.x);
 	l.b = 1 / (ellipsoid.size.y * ellipsoid.size.y);
@@ -80,7 +45,7 @@ static t_locals	s_locals(t_ray ray, t_map_ellipsoid ellipsoid)
 	return (l);
 }
 
-static t_err	front(t_locals l, t_ray ray, t_ray_hit_records_builder *builder)
+static t_err	front(t_vars l, t_ray ray, t_ray_hit_records_builder *builder)
 {
 	const t_f				distance = (-l.y - l.sqrt_y2_4xz) / (2 * l.x);
 	const t_map_position	point
@@ -92,7 +57,7 @@ static t_err	front(t_locals l, t_ray ray, t_ray_hit_records_builder *builder)
 	if (distance <= 0)
 		return (t_ray_hit_records_builder_add(
 				builder, t_ray_default_hit_record(l.self.material)));
-	normal = t_ray_primitive_ellipsoid_normal(point);
+	normal = t_ray_primitive_ellipsoid_normal(l, point);
 	t_ray_primitive_ellipsoid_coord(point, l.self.size, &coord_x, &coord_y);
 	return (
 		t_ray_hit_records_builder_add(builder, (t_ray_hit_record){
@@ -106,7 +71,7 @@ static t_err	front(t_locals l, t_ray ray, t_ray_hit_records_builder *builder)
 	);
 }
 
-static t_err	back(t_locals l, t_ray ray, t_ray_hit_records_builder *builder)
+static t_err	back(t_vars l, t_ray ray, t_ray_hit_records_builder *builder)
 {
 	const t_f				distance = (-l.y + l.sqrt_y2_4xz) / (2 * l.x);
 	const t_map_position	point
@@ -115,7 +80,7 @@ static t_err	back(t_locals l, t_ray ray, t_ray_hit_records_builder *builder)
 	t_f						coord_x;
 	t_f						coord_y;
 
-	normal = t_ray_primitive_ellipsoid_normal(point);
+	normal = t_ray_primitive_ellipsoid_normal(l, point);
 	t_ray_primitive_ellipsoid_coord(point, l.self.size, &coord_x, &coord_y);
 	return (
 		t_ray_hit_records_builder_add(builder, (t_ray_hit_record){
@@ -135,7 +100,7 @@ t_err	t_ray_primitive_ellipsoid_record(
 	t_ray_hit_records_builder *builder
 )
 {
-	const t_locals	l = s_locals(ray, ellipsoid);
+	const t_vars	l = s_vars(ray, ellipsoid);
 
 	if (!l.hit)
 		return (false);
