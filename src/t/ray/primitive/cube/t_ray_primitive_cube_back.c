3391d49521f939c1e4d4a3055f0d84b69a73ebff
yeonseong <yeonseong.dev@gmail.com>
2023-02-06 09:45:58 +0900

    wip: cube 고치는 중

--- src/t/ray/primitive/cube/t_ray_primitive_cube_back.c
+++ src/t/ray/primitive/cube/t_ray_primitive_cube_back.c
@@ -16,13 +16,47 @@
 #include "t_map.h"
 #include "t_ray.h"
 
+typedef struct s_locals
+{
+	t_f				distance;
+	t_map_position	point;
+	t_f				x;
+	t_f				y;
+	bool			is_front_face;
+}	t_locals;
+
+static bool is_inside(t_map_cube cube, t_map_position point)
+{
+	if (point.y < cube.position.y || point.y > cube.position.y + cube.size.y)
+		return (false);
+	if (point.z < cube.position.z || point.z > cube.position.z + cube.size.z)
+		return (false);
+	return (true);
+}
+
 t_err	t_ray_primitive_cube_back(
 			t_ray ray,
 			t_map_cube cube,
 			t_ray_hit_records_builder *builder)
 {
-	(void) ray;
-	(void) cube;
-	(void) builder;
-	return (false);
+	t_locals	l;
+
+	// plan's normal : (-1,0,0)
+	l.distance = - (ray.origin.x * -1 - cube.position.x) / (ray.direction.x * -1);
+	l.point = t_f3_add(ray.origin, t_f3_mul(ray.direction, l.distance));
+
+	l.x = l.point.x / cube.size.x;
+	l.y = l.point.y / cube.size.y;
+	if (l.distance < 0 || !is_inside(cube, l.point))
+		return (false);
+	l.is_front_face = ray.direction.z > 0;
+	return (t_ray_hit_records_builder_add(builder, (t_ray_hit_record){
+		l.distance,
+		(t_map_normal){-1, 0, 0},
+		t_ray_material_from_color(cube.material_back),
+		l.is_front_face,
+		l.x,
+		l.y
+		})
+	);
 }
