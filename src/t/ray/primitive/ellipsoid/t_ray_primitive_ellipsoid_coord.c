cb9517edea6780b14313502f473d80662c222329
맹주영 <mjy9088@naver.com>
2023-02-08 23:51:29 +0900

    correct sphere texture coord

--- src/t/ray/primitive/ellipsoid/t_ray_primitive_ellipsoid_coord.c
+++ src/t/ray/primitive/ellipsoid/t_ray_primitive_ellipsoid_coord.c
@@ -26,5 +26,5 @@ void	t_ray_primitive_ellipsoid_coord(
 	const t_f3	p = t_f3_div3(point, size);
 
 	*out_x = t_f_rot(t_f_atan2(p.y, p.x));
-	*out_y = t_f_rot(t_f_asin(p.z));
+	*out_y = (t_f_rot(t_f_asin(p.z)) + (t_f)0.25) * (t_f)2;
 }
